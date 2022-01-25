016ec776d58592fbb70ddc5d3b07a47151d2d6cd
Joshie112358 <joshuabarceinas@gmail.com>
2022-01-25 00:40:20 +0000

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,147 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, modulePath, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "shiva"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "America/Mexico_City";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.wlp2s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    interactiveShellInit = ''
+      # z - jump around
+      # source ${pkgs.fetchurl {url = "https://github.com/rupa/z/raw/2ebe419ae18316c5597dd5fb84b5d8595ff1dde9/z.sh"; sha256 = "0ywpgk3ksjq7g30bqbhl9znz3jh6jfg8lxnbdbaiipzgsy41vi10";}}
+      save_aliases=$(alias -L)
+      export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh
+      export ZSH_THEME="bira" #"lambda"
+      plugins=(git sudo colorize extract history postgres)
+      source $ZSH/oh-my-zsh.sh
+      eval $save_aliases; unset save_aliases
+    '';
+    promptInit = ''
+      any-nix-shell zsh --info-right | source /dev/stdin
+  '';
+  };
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.jane = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  # };
+
+  # List packages installed in system profile. To search, run:
+   #$ nix search wget
+   environment.systemPackages = with pkgs; [
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget
+     firefox
+     emacs
+     zsh
+   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # For nix flakes
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = "experimental-features = nix-command flakes";
+
+  users.mutableUsers = false;
+
+  # Password generated with ```mkpasswd -m sha-512```
+  users.users.root.initialHashedPassword = "$6$y1n2g52P63iUa5b.$9RS3Q2eahVKH4HuvJVNK/Iyj1QF2ctP2dtDI4ko52ZgZxfkhW4pWPbTtMOf/Bvihwsro1aBVxstoxUBuT1lcM.";
+  users.users.joshuabc.initialHashedPassword = "$6$y1n2g52P63iUa5b.$9RS3Q2eahVKH4HuvJVNK/Iyj1QF2ctP2dtDI4ko52ZgZxfkhW4pWPbTtMOf/Bvihwsro1aBVxstoxUBuT1lcM."; # this may be redundant
+  # users.defaultUserShell = pkgs.zsh;
+  users.extraUsers.joshuabc = {
+    createHome = true;
+    isNormalUser = true;
+    home = "/home/joshuabc";
+    description = "Joshua Barceinas";
+    extraGroups = [ "video" "wheel" "networkmanager" "docker" ];
+    hashedPassword = "$6$y1n2g52P63iUa5b.$9RS3Q2eahVKH4HuvJVNK/Iyj1QF2ctP2dtDI4ko52ZgZxfkhW4pWPbTtMOf/Bvihwsro1aBVxstoxUBuT1lcM.";
+    shell = pkgs.zsh; #"/run/current-system/sw/bin/bash";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+}
+
+
